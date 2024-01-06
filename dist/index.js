d4ca1550273c022dfb1870fea1dfba088cd7cb2c
Shakib Jilani <64030706+Shakib40@users.noreply.github.com>
2024-01-07 02:16:11 +0530

    Updated

---
+++ dist/index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Input", {
+  enumerable: true,
+  get: function get() {
+    return _Input.Input;
+  }
+});
+Object.defineProperty(exports, "Textarea", {
+  enumerable: true,
+  get: function get() {
+    return _Textarea.Textarea;
+  }
+});
+var _Input = require("./Hooks/Input");
+var _Textarea = require("./Hooks/Textarea");
