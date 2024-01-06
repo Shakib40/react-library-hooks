ed5fcbb69506eeb683b7a00dbb93dfd1849a4126
Shakib Jilani <64030706+Shakib40@users.noreply.github.com>
2024-01-07 02:26:20 +0530

    Updated

--- dist/Hooks/Input.js
+++ dist/Hooks/Input.js
@@ -7,17 +7,46 @@ exports.Input = void 0;
 var _react = _interopRequireDefault(require("react"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 var Input = exports.Input = function Input(_ref) {
-  var _ref$placeholder = _ref.placeholder,
+  var label = _ref.label,
+    _ref$placeholder = _ref.placeholder,
     placeholder = _ref$placeholder === void 0 ? "Enter" : _ref$placeholder,
     _ref$disabled = _ref.disabled,
     disabled = _ref$disabled === void 0 ? false : _ref$disabled,
     value = _ref.value,
-    onChange = _ref.onChange;
-  return /*#__PURE__*/_react["default"].createElement("div", null, /*#__PURE__*/_react["default"].createElement("input", {
+    onChange = _ref.onChange,
+    onFocus = _ref.onFocus,
+    onBlur = _ref.onBlur,
+    onKeyDown = _ref.onKeyDown,
+    errorMessage = _ref.errorMessage,
+    showClearButton = _ref.showClearButton,
+    onClear = _ref.onClear;
+  var handleFocus = function handleFocus(e) {
+    if (onFocus) {
+      onFocus(e);
+    }
+  };
+  var handleBlur = function handleBlur(e) {
+    if (onBlur) {
+      onBlur(e);
+    }
+  };
+  var handleKeyDown = function handleKeyDown(e) {
+    if (onKeyDown) {
+      onKeyDown(e);
+    }
+  };
+  return /*#__PURE__*/_react["default"].createElement("div", null, label && /*#__PURE__*/_react["default"].createElement("label", null, label), /*#__PURE__*/_react["default"].createElement("input", {
     type: "text",
     placeholder: placeholder,
     disabled: disabled,
     value: value,
-    onChange: onChange
-  }));
+    onChange: onChange,
+    onFocus: handleFocus,
+    onBlur: handleBlur,
+    onKeyDown: handleKeyDown
+  }), errorMessage && /*#__PURE__*/_react["default"].createElement("div", {
+    style: {
+      color: "red"
+    }
+  }, errorMessage));
 };
--- src/Hooks/Input.js
+++ src/Hooks/Input.js
@@ -1,20 +1,50 @@
 import React from "react";
 
 export const Input = ({
+  label,
   placeholder = "Enter",
   disabled = false,
   value,
   onChange,
+  onFocus,
+  onBlur,
+  onKeyDown,
+  errorMessage,
+  showClearButton,
+  onClear,
 }) => {
+  const handleFocus = (e) => {
+    if (onFocus) {
+      onFocus(e);
+    }
+  };
+
+  const handleBlur = (e) => {
+    if (onBlur) {
+      onBlur(e);
+    }
+  };
+
+  const handleKeyDown = (e) => {
+    if (onKeyDown) {
+      onKeyDown(e);
+    }
+  };
+
   return (
     <div>
+      {label && <label>{label}</label>}
       <input
         type="text"
         placeholder={placeholder}
         disabled={disabled}
         value={value}
         onChange={onChange}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        onKeyDown={handleKeyDown}
       />
+      {errorMessage && <div style={{ color: "red" }}>{errorMessage}</div>}
     </div>
   );
 };
